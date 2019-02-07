f806545c395e59b344a919e9f278e30fd763e585
Sreejith <ssreedharan@acr.org>
2019-02-07 19:41:40 +0530

    Code chnages

---
+++ RadElement.API/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RadElement.API.Controllers
+{
+    /// <summary>
+    /// Endpoint for hello worls controller
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.Controller" />
+    [Produces("application/json")]
+    [Route("api/HelloWorld")]
+    public class HelloWorldController : Controller
+    {
+        /// <summary>
+        /// Method used to check if service is running
+        /// </summary>
+        /// <returns>Returns "Hello World" </returns>
+        [Microsoft.AspNetCore.Authorization.AllowAnonymous]
+        [HttpGet]
+        public IActionResult HelloWorld()
+        {
+            return Ok("Hello  World");
+        }
+    }
+}
