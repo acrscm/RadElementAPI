41d266a8a9e3e46ce586f28dc6e3eda4b0754725
Sreejith <ssreedharan@acr.org>
2019-02-11 17:01:17 +0530

    Code changes

---
+++ RadElement.Core/DTO/ElementType.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace RadElement.Core.DTO
+{
+    /// <summary>
+    /// Gets or sets the Data element type
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum ElementType
+    {
+        /// <summary>
+        /// Data Element Type is Integer
+        /// </summary>
+        Integer,
+
+        /// <summary>
+        /// Data Element Type is Numeric
+        /// </summary>
+        Numeric,
+
+        /// <summary>
+        /// Data Element Type is Choice
+        /// </summary>
+        Choice,
+
+        /// <summary>
+        /// Data Element Type is MultiChoice
+        /// </summary>
+        MultiChoice
+    }
+}
