948f00163752eafba05c6e9290004222d8481622
Sreejith <SSreedharan@acr.org>
2020-03-30 17:44:51 +0530

    Removed the unused files

--- RadElement.Core/Domain/DataElement.cs
+++ RadElement.Core/Domain/DataElement.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-namespace RadElement.Core.Domain
+﻿namespace RadElement.Core.Domain
 {
     /// <summary>
     /// Base class for all data elements
@@ -16,75 +14,5 @@ namespace RadElement.Core.Domain
         /// Gets of sets the type
         /// </summary>
         public DataElementType DataElementType { get; protected set; }
-
-        /// <summary>
-        /// Gets or sets the label of the data element
-        /// </summary>
-        public string Label { get; set; }
-
-        /// <summary>
-        /// Gets or sets if the element is required
-        /// </summary>
-        public bool Required { get; set; }
-
-        /// <summary>
-        /// Gets or sets the hint text
-        /// </summary>
-        public string HintText { get; set; }
-
-        /// <summary>
-        /// Represents the unit
-        /// </summary>
-        public string Unit { get; set; }
-
-        /// <summary>
-        /// Gets or sets the diagram collection
-        /// </summary>
-        public List<Diagram> Diagrams { get; set; }
-
-        /// <summary>
-        /// Gets of sets the display sequnce
-        /// </summary>
-        public int DisplaySequence { get; set; }
-
-        /// <summary>
-        /// Gets or sets the values 
-        /// </summary>
-        public string Value { get; set; }
-
-        /// <summary>
-        /// Gets or sets the voice command.
-        /// </summary>
-        public string VoiceCommand { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether this <see cref="DataElement"/> is editable.
-        /// </summary>
-        public bool Editable { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether this <see cref="DataElement"/> is hasprefilled.
-        /// </summary>
-        public bool Hasprefilled { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether this <see cref="DataElement"/> is output.
-        /// </summary>
-        public bool Output { get; set; }
-
-        /// <summary>
-        /// Gets or sets the source.
-        /// </summary>
-        public Source Source { get; set; }
-
-        /// <summary>
-        /// Gets or sets the codable concept.
-        /// </summary>
-        public CodableConcept CodableConcept { get; set; }
-
-        public DataElement()
-        {
-            CodableConcept = new CodableConcept();
-        }
     }
 }
