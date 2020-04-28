e6d607a2ece3dbbf5dcca2b607e647d747086469
Sreejith <SSreedharan@acr.org>
2020-04-28 13:08:41 +0530

    Added more info to reponse of element creation

--- RadElement.Core/DTO/ElementIdDetails.cs
+++ RadElement.Core/DTO/ElementIdDetails.cs
@@ -6,5 +6,10 @@
         /// Gets or sets the element identifier.
         /// </summary>
         public string ElementId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the message.
+        /// </summary>
+        public string Message { get; set; }
     }
 }
